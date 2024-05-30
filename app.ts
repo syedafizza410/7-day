974eaad957764013c3dec7354c4df83b85b59adc
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2024-05-31 03:07:26 +0500

    add

---
+++ app.ts
@@ -0,0 +1,32 @@
+// ************* 100 coding challenge "7 day" ************** //
+
+
+/* Q19 Dinner Guests: Use one of your programs from Exercises 14 through 
+18 to print a message indicating the number of people you are inviting 
+to dinner.*/
+
+let guests6: string[] = ["Huda", "Bakhtu", "Saba", "Hina", "Amina"];
+console.log(`I am inviting ${guests6.length} people to dinner.`);
+
+/* Q20 Think of something you could store in an array. For example, you 
+could make a list of mountains, rivers, countries, cities, languages, or 
+anything else you’d like. Write a program that creates a list containing 
+these items.*/
+
+let languages: string[] = ["Saraiki", "German", "Turkish", "Arabic", "Sindhi"];
+console.log("Languages I'd like to learn:", languages);
+
+/* Q21 Think of something you could store in a TypeScript Object. 
+Write a program that creates Objects containing these items.*/
+
+let car:{
+        Name: string; 
+        Model: string; 
+        Price: number
+    } = {
+
+        Name: "Toyota Corolla",
+        Model: "Gli auto 2024",
+        Price: 2200000,
+    };
+console.log(`Car Name: ${car.Name}, Model No: ${car.Model}, Price: ${car.Price}`);
